7f5d4d91f00a8b50dbb724cbbc7b35f54f5c2d10
AClaich <claich.alexis@gmail.com>
2022-02-20 12:42:59 +0100

    put in place

--- src/app/mobile-container/mobile-container.component.ts
+++ src/app/mobile-container/mobile-container.component.ts
@@ -1,9 +1,24 @@
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-container',
   templateUrl: './mobile-container.component.html',
-  styleUrls: ['./mobile-container.component.scss']
+  styleUrls: ['./mobile-container.component.scss'],
+  animations: [
+    trigger('Opening', [
+      transition(':enter', [
+        query('.logo-img', [
+          style({ opacity: 0, transform: 'translateY(20px)'}),
+          stagger(50, [
+            animate('500ms', style({ opacity: 1, transform: 'translateY(0px)'}))
+          ]),
+        ])
+        // style({ opacity: 0, transform: 'translateY(20px)'}),
+        // animate('500ms', style({ opacity: 1, transform: 'translateY(0px)'})),
+      ])
+    ])
+  ]
 })
 export class MobileContainerComponent implements OnInit {
 
